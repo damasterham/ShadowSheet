0a94a4e04828ae691d28bbc0dcd99a16babb446f
damasterham <cmeisterham@gmail.com>
2017-05-18 13:08:10 +0200

    Spring Boot boiler plate and project structure.

---
+++ src/main/java/runner/Application.java
@@ -0,0 +1,18 @@
+package runner;//
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//Created by DaMasterHam on 27-04-2017.
+//
+@EnableAutoConfiguration
+@SpringBootApplication
+public class Application
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/test/java/runner/UnitTest.java
@@ -0,0 +1,18 @@
+package runner;//Created by DaMasterHam on 27-04-2017.
+//
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UnitTest
+{
+    @Test
+    public void Test()
+    {
+        assertTrue(true);
+    }
+
+
+
+}
