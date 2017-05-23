fed0f90ad9b916c5ab47e5665d3419e8b8b7c62e
damasterham <cmeisterham@gmail.com>
2017-05-23 15:58:30 +0200

    Added a new test, but have become uncertain if it is necessary or even valuable to "Unit Test" the repository.

--- src/test/java/api/RunnerRepoTest.java
+++ src/test/java/api/RunnerRepoTest.java
@@ -1,37 +1,39 @@
 package api;//
 
 import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
 
 import api.mock.RunnerRepoMockSimple;
-import beans.Attributes;
-import beans.PrivateInfo;
-import beans.Runner;
+import api.repo.RunnerRepo;
+import api.beans.Attributes;
+import api.beans.PersonalInfo;
+import api.beans.Runner;
 
-import beans.Skills;
-import org.assertj.core.util.IterableUtil;
+import api.beans.Skills;
 import org.assertj.core.util.Lists;
 import org.junit.Before;
 import org.junit.Test;
-import repo.RunnerRepo;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 //Created by DaMasterHam on 23-05-2017.
 //
 public class RunnerRepoTest
 {
-    private RunnerRepo repo = new RunnerRepoMockSimple();
+    private RunnerRepo repo;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        repo = new RunnerRepoMockSimple();
+    }
 
     @Test
     public void GetAllRunnersSimple() throws Exception
     {
         Iterable<Runner> runners = Arrays.asList(
-                new Runner(0, new PrivateInfo(), new Attributes(), new Skills()),
-                new Runner(1, new PrivateInfo(), new Attributes(), new Skills()),
-                new Runner(2, new PrivateInfo(), new Attributes(), new Skills())
+                new Runner(0, new PersonalInfo(), new Attributes(), new Skills()),
+                new Runner(1, new PersonalInfo(), new Attributes(), new Skills()),
+                new Runner(2, new PersonalInfo(), new Attributes(), new Skills())
         );
 
         Iterable<Runner> restRunners = repo.findAll();
@@ -45,7 +47,7 @@ public class RunnerRepoTest
     @Test
     public void GetRunnerZeroSimple() throws Exception
     {
-        Runner runner = new Runner(0, new PrivateInfo(), new Attributes(), new Skills());
+        Runner runner = new Runner(0, new PersonalInfo(), new Attributes(), new Skills());
         Runner fromRest = repo.findById(0);
 
         assertEquals(runner,fromRest);
@@ -54,10 +56,18 @@ public class RunnerRepoTest
     @Test
     public void CreateRunnerSimple() throws Exception
     {
-        Runner newGuy = new Runner(new PrivateInfo(), new Attributes(), new Skills());
+        Runner newGuy = new Runner(new PersonalInfo(), new Attributes(), new Skills());
 
         Runner created = repo.save(newGuy);
 
         assertEquals(1, created.getId());
+    }
+
+    @Test
+    public void AddPersonalInfoToRunnerZero() throws Exception
+    {
+        Runner zero = repo.findById(0);
+
+        assertFalse(true);
     }
 }
---
+++ src/test/java/api/mock/RunnerRepoMockSimple.java
@@ -0,0 +1,100 @@
+package api.mock;//
+
+import api.beans.Attributes;
+import api.beans.PersonalInfo;
+import api.beans.Runner;
+import api.beans.Skills;
+import api.repo.RunnerRepo;
+
+import java.util.Arrays;
+
+//Created by DaMasterHam on 23-05-2017.
+//
+public class RunnerRepoMockSimple implements RunnerRepo
+{
+    private static Iterable<Runner> runners = Arrays.asList(
+            new Runner(0, new PersonalInfo(), new Attributes(), new Skills()),
+            new Runner(1, new PersonalInfo(), new Attributes(), new Skills()),
+            new Runner(2, new PersonalInfo(), new Attributes(), new Skills())
+    );
+
+    private static Runner zero = new Runner(0, new PersonalInfo(), new Attributes(), new Skills());
+
+
+    @Override
+    public <S extends Runner> S save(S s)
+    {
+        s.setId(1);
+        return s;
+    }
+
+    @Override
+    public <S extends Runner> Iterable<S> save(Iterable<S> iterable)
+    {
+        return null;
+    }
+
+    @Override
+    public Runner findOne(Long aLong)
+    {
+       /* if (aLong == 0)
+            return zero;*/
+       return null;
+    }
+
+    @Override
+    public Runner findById(long id)
+    {
+        if (id == 0)
+            return zero;
+        return null;
+    }
+
+    @Override
+    public boolean exists(Long aLong)
+    {
+        return false;
+    }
+
+    @Override
+    public Iterable<Runner> findAll()
+    {
+        return runners;
+    }
+
+    @Override
+    public Iterable<Runner> findAll(Iterable<Long> iterable)
+    {
+        return null;
+    }
+
+    @Override
+    public long count()
+    {
+        return 0;
+    }
+
+    @Override
+    public void delete(Long aLong)
+    {
+
+    }
+
+    @Override
+    public void delete(Runner runner)
+    {
+
+    }
+
+    @Override
+    public void delete(Iterable<? extends Runner> iterable)
+    {
+
+    }
+
+    @Override
+    public void deleteAll()
+    {
+
+    }
+}
