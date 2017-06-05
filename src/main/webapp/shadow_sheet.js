3083253f04eb1ff8c39a118e2de88c799526b222
damasterham <cmeisterham@gmail.com>
2017-06-05 22:54:02 +0200

    Final additions when project ended. Added a RunnerController which has custom logic for adding PersonalInfo, with Aliases to a Runner. Added Initial try at Angular.

--- src/main/webapp/shadow_sheet.js
+++ src/main/webapp/shadow_sheet.js
@@ -1,3 +1,32 @@
 /**
  * Created by DaMasterHam on 26-05-2017.
  */
+
+var root = 'http://localhost:8080'
+var shadowsheet = angular.module('shadowsheet', [])
+
+shadowsheet.controller('RunnerController', function RunnerController($scope, $http)
+{
+    $scope.endpoints[
+        {
+            name: 'Create Runner',
+            url: '/api/runners',
+            method: 'POST'
+        }];
+
+    $scope.createRunner(function ($http)
+    {
+        $http.post(root + "/api/runners", "{}")
+            .then(function(response) {
+                $scope.runnerendpoints = response.data;
+            });
+    });
+});
+
+shadowsheet.controller('RunnerController',  function($scope, $http) {
+        $http.get(root).
+        then(function(response) {
+            $scope.greeting = response.data;
+        });
+    });
+
